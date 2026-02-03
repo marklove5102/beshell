6dc97efb51d8c6393fd2d53f0dbb7d3898b856fa
alee <alee201@163.com>
2026-02-03 18:20:22 +0800

    change version

--- src/beshell/BeShell.class.hpp
+++ src/beshell/BeShell.class.hpp
@@ -8,7 +8,7 @@
 #include "./driver/DriverModule.hpp"
 #include "./deps/quickjs/quickjs.h"
 
-#define BESHELL_VERSION "1.0.5"
+#define BESHELL_VERSION "1.0.6"
 #define ESPIDF_VERSION IDF_VER
 
 namespace be {
